3a26db038a162f2f7ba6b228fd8bdfc20577ca21
Larsen Eisenberg <magiclarsen@gmail.com>
2016-04-16 20:45:39 -0400

    Initial Commit

---
+++ pokedex-by-me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-me
+//
+//  Created by Larsen Eisenberg on 4/16/16.
+//  Copyright © 2016 Magic By Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
