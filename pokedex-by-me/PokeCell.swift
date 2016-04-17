bc62b4691cca7979b5ddc0776b64ffa19672af24
Larsen Eisenberg <magiclarsen@gmail.com>
2016-04-16 22:24:04 -0400

    Created the Pokemon Class and the PokeCell for the Collection View, displayed the CollectionView on the screen and completed some of the layout for the app.

---
+++ pokedex-by-me/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex-by-me
+//
+//  Created by Larsen Eisenberg on 4/16/16.
+//  Copyright © 2016 Magic By Larsen. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
