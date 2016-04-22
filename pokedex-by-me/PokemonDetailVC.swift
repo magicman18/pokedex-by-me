9bbcc4861acf96419f2222622152dabc26197eb2
magicman18 <magiclarsen@gmail.com>
2016-04-22 10:35:24 -0700

    Set up layout of the Detail View, added more variables to Pokemon Class, added IBOutlets and IBAction to the PokemonDetailVC

--- pokedex-by-me/PokemonDetailVC.swift
+++ pokedex-by-me/PokemonDetailVC.swift
@@ -11,6 +11,16 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var EvoLbl: UILabel!
     
     var pokemon: Pokemon!
     
@@ -26,15 +36,10 @@ class PokemonDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+
+   
 
 }
